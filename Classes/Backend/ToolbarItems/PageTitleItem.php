8348463bc733e87112a0a7aa7941886c8fbc7ad5
Konrad Michalik <km@move-elevator.de>
2026-07-27 17:08:11 +0200

    fix: add CSP nonce to backend page title inline script

--- Classes/Backend/ToolbarItems/PageTitleItem.php
+++ Classes/Backend/ToolbarItems/PageTitleItem.php
@@ -53,7 +53,7 @@ class PageTitleItem implements ToolbarItemInterface
             return '';
         }
 
-        $this->pageRenderer->addJsInlineCode(Configuration::EXT_KEY.'_pagetitle', $this->buildScript($prefix, $suffix));
+        $this->pageRenderer->addJsInlineCode(Configuration::EXT_KEY.'_pagetitle', $this->buildScript($prefix, $suffix), null, false, true);
 
         return '';
     }
