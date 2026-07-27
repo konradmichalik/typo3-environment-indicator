663a895d54c459e6d1ed67f137305b60058e9d61
Konrad Michalik <km@move-elevator.de>
2026-07-27 17:47:35 +0200

    fix: attach CSP nonce to topbar description inline script

--- Classes/Backend/ToolbarItems/TopbarItem.php
+++ Classes/Backend/ToolbarItems/TopbarItem.php
@@ -79,6 +79,7 @@ class TopbarItem implements ToolbarItemInterface
                     'document.querySelector(".topbar")?.setAttribute("title",%s);',
                     json_encode($description, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_THROW_ON_ERROR),
                 ),
+                csp: true,
             );
         }
 
